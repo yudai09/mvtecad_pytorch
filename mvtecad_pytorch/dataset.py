a7f9af2901d5b13e972a0dd4bb52d0c9d2e5045d
加藤 雄大 <y-kato@chowagiken.co.jp>
2021-04-06 23:57:46 +0900

    add docstring

--- mvtecad_pytorch/dataset.py
+++ mvtecad_pytorch/dataset.py
@@ -2,10 +2,33 @@ from torchvision.datasets import VisionDataset
 from PIL import Image
 import glob
 import os
+from typing import Callable, Optional, List
 
 
 class MVTecADDataset(VisionDataset):
-    def __init__(self, root, target, train, transforms=None, mask_transforms=None, img_ext=".png"):
+    """`MVTecAD <https://www.mvtec.com/company/research/datasets/mvtec-ad>`_ Dataset.
+
+    Args:
+        root (str): path to data directory contains MVTecAD
+        target (str): target object. e.g. bottle, cable, capsule.
+        train (bool): choise of train data or test data.
+        transforms (Optional[Callable], optional): A function/transform that takes in an PIL image and returns a transformed version. E.g, ``transforms.ToTensor``
+        mask_transforms (Optional[Callable], optional): A function/transform that takes in the mask and transforms it.
+        img_ext (str, optional): image file extension that are to be loaded. Defaults to ".png".
+
+    Raises:
+        FileNotFoundError: [description]
+    """
+
+    def __init__(
+        self,
+        root: str,
+        target: str,
+        train: bool,
+        transforms: Optional[Callable] = None,
+        mask_transforms: Optional[Callable] = None,
+        img_ext: str = ".png",
+    ):
         self.root = root
         self.transforms = transforms
         self.mask_transforms = mask_transforms
@@ -44,10 +67,10 @@ class MVTecADDataset(VisionDataset):
 
         return img, mask, label
 
-    def _load_image(self, idx):
+    def _load_image(self, idx: int) -> Image.Image:
         return Image.open(self.img_path_list[idx])
 
-    def _load_mask(self, idx, img):
+    def _load_mask(self, idx: int, img: Image.Image) -> Image.Image:
         path = self.mask_path_list[idx]
         label = self.label_list[idx]
 
@@ -62,24 +85,24 @@ class MVTecADDataset(VisionDataset):
 
         return mask
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.img_path_list)
 
-    def _get_target_list(self):
+    def _get_target_list(self) -> List[str]:
         return os.listdir(self.root)
 
-    def _get_labels(self, target_root):
+    def _get_labels(self, target_root: str) -> List[str]:
         labels = os.listdir(target_root)
         # append `good' at beggning of class list to give it label 0
         labels.pop(labels.index("good"))
         labels = ["good"] + labels
         return labels
 
-    def _get_label_from_path(self, img_path):
+    def _get_label_from_path(self, img_path: str) -> str:
         img_path_elems = img_path.split(os.sep)
         return img_path_elems[-2]
 
-    def _get_mask_path(self, img_path, img_ext):
+    def _get_mask_path(self, img_path: str, img_ext: str) -> str:
         img_path_elems = img_path.split(os.sep)
         img_path_elems[-3] = "ground_truth"
         img_path_elems[-1] = img_path_elems[-1].split(img_ext)[0]
